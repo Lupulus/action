d5bf553708bdbfacf7edbfed557e480395503bda
Lupulus <timothee.laval@gmail.com>
2015-10-06 11:43:24 +0200

    ajout de commentaire sur la classe Scheduler

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -9,21 +9,33 @@ public class Scheduler extends ActionComponent {
 	}
 
 	/**
-	 * 
+	 * return true is isInitialized and isReady are true, return false if not
 	 * @return boolean
 	 */
 	public boolean isReady(){
-		return super.isInitialized && isReady();
+		return super.isInitialized && super.isReady;
 	}
-	
+	/**
+	 * return true is isInitialized, isReady() and isFinished are true, false if not
+	 * @return boolean
+	 */
 	public boolean isInProgress(){
 		return super.isInitialized && !isReady() && !isFinished();
 	}
 	
+	/**
+	 * return true if isInitiliazed, isReady are true and if the ArrayList actions is at the end
+	 * return false if not
+	 * @return boolean
+	 */
 	public boolean isFinished(){
 		return super.isInitialized && !isReady() && actions.isEmpty();
 	}
 	
+	
+	/**
+	 * 
+	 */
 	public void doStep(){
 		super.isReady = false;
 		ActionComponent nextAction = super.actions.get(0);
