2462e415b732ce6866715034247a8fe039000726
RijselFlo <floloridan@gmail.com>
2015-10-06 11:45:19 +0200

    ajout de la classe foreseable

---
+++ src/Foreseable.java
@@ -0,0 +1,23 @@
+
+public class Foreseable extends ActionComponent {
+
+	public Foreseable(int timeToEnd){
+		super(timeToEnd);
+	}
+	
+	public boolean isReady(){
+		return remainingTime == totalTime;
+	}
+	
+	public boolean isInProgress(){
+		return !isReady() && !isFinished();
+	}
+	
+	public boolean IsFinished(){
+		return remainingTime <= 0;
+	}
+	
+	public void doStep(){
+		remainingTime--;
+	}
+}
